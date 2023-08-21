ac369fff96233e439b741e7fa765fd9f260f33c6
Ryo Ono <85672766+s1F102102245@users.noreply.github.com>
2023-08-21 14:36:40 +0900

    APIキー取得

--- gptapp/views.py
+++ gptapp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# import openai
+
+# openai.api_key = 'YdtsmLm1tUQi2x5uiUlmyISUTJ_ZiP-EOwlmiMOyM0bjz6iun2gUnO6TszHRCb10O_QsmQwLZ-i7kH8h5rJ9Mww'
+# openai.api_base = 'https://api.openai.iniad.org/api/v1'
+
 
 # Create your views here.
 
@@ -21,4 +26,3 @@ def param(request):
 
 def index(request):
     return render(request, 'gptapp/index.html')
-
