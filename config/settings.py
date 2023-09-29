b1a39a53c66687cab9fc55eff01d5ddf9a1df257
Ryo Ono <85672766+s1F102102245@users.noreply.github.com>
2023-09-29 12:01:01 +0900

    アプリ追加

--- config/settings.py
+++ config/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gptapp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -159,5 +161,5 @@ LOGGING = {
 
 
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 
