cb910244c07cf2c7b22eda64f6dc4933deb56bdc
s1f102102379 <85721024+s1f102102379@users.noreply.github.com>
2023-11-23 15:17:47 +0900

    change settintgs
    security

---
+++ config/local_settings.py
@@ -0,0 +1,4 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-k4wmb6zn7*m=c+c9wi(o*_d1^9$wy8j9r%xo1&qn74q=*mc%po'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from .local_settings import *
 from pathlib import Path
 import os
 import openai
@@ -15,10 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TESSERACT_CMD = os.path.join(BASE_DIR, 'Tesseract-OCR', 'tesseract.exe')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k4wmb6zn7*m=c+c9wi(o*_d1^9$wy8j9r%xo1&qn74q=*mc%po'
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 ALLOWED_HOSTS = ['3.112.171.82']
 ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
