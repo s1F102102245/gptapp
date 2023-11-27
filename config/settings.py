efb34aa5c44cbc2d3cf2c5b66f4045060f819937
s1f102102379 <85721024+s1f102102379@users.noreply.github.com>
2023-11-27 16:55:54 +0900

    diss

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,6 @@ import pytesseract
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TESSERACT_CMD = os.path.join(BASE_DIR, 'Tesseract-OCR', 'tesseract.exe')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
