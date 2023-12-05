caf1a4fdb5a5859dfed9ee6559993105d1890bdd
s1f102102852 <85666707+s1f102102852@users.noreply.github.com>
2023-12-06 06:42:27 +0900

    button

--- config/forms.py
+++ config/forms.py
@@ -2,4 +2,4 @@ from django import forms
 import openai
 
 class ChatForm(forms.Form):
-    user_input = forms.CharField(label='あなたの質問', max_length=300, widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}))
+    user_input = forms.CharField(label='質問はあるワン？', max_length=300, widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}))
