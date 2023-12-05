f800ef5e926e09775fd3edf798fdc4213288da6f
EC2 Default User <ec2-user@ip-10-0-1-10.ap-northeast-1.compute.internal>
2023-12-05 09:58:58 +0000

    config/wsgi.pyの変更をコミット

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# 追加
+sys.path.append('/home/ec2-user/gptapp/config')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
