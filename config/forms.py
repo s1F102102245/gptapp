e99a1279820f62064cf27d8f48fa51998134454e
s1f102102852 <85666707+s1f102102852@users.noreply.github.com>
2023-12-06 08:55:41 +0900

    formの大きさ変更と調整と

--- config/forms.py
+++ config/forms.py
@@ -2,4 +2,9 @@ from django import forms
 import openai
 
 class ChatForm(forms.Form):
-    user_input = forms.CharField(label='質問はあるワン？', max_length=300, widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}))
+    user_input = forms.CharField(max_length=300, 
+                    
+                    widget=forms.Textarea(attrs={
+                            'rows': 4,
+                            'cols': 50,
+                            'style':'resize:none;',}))
