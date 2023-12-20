802584e1b95470400a570a053d68a110f5d898cc
Ryo Ono <85672766+s1F102102245@users.noreply.github.com>
2023-12-20 18:07:56 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ python manage.py migrate
 #python manage.py createsuperuser --noinput
 
 poetry run gunicorn gptapp.wsgi:application --bind 0.0.0.0:8000
+
+gunicorn gptapp.wsgi:application --bind 0.0.0.0:8000
