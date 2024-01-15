5b2637b45a9477beda043a4b9ebec3a317e78142
1F10210190F <85672606+1F10210190F@users.noreply.github.com>
2024-01-15 22:55:23 +0900

    調整1

--- gptapp/forms.py
+++ gptapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ChatForm(forms.Form):
-    user_input = forms.CharField(label='あなたの質問', max_length=300, widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}))
+    user_input = forms.CharField(label='', max_length=300, widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}))
 
 
 class ImageUploadForm(forms.Form):
